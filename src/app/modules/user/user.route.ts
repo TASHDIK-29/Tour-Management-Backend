08005653cc65ad9f4901b88cce78be9cfbc9a1e0
TASHDIK-29 <trk.ice153@gmail.com>
2026-03-31 01:51:32 +0600

    part-3 and part-4 complete

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -10,7 +10,9 @@ const router = Router();
 
 
 
-router.post('/register', validateRequest(createUserZodSchema), UserControllers.createUser);
+router.post('/register',
+    validateRequest(createUserZodSchema),
+    UserControllers.createUser);
 router.get('/all-users', checkAuth(Role.ADMIN, Role.SUPER_ADMIN), UserControllers.getAllUsers);
 router.patch('/:id', validateRequest(updateUserZodSchema), checkAuth(...Object.values(Role)), UserControllers.updateUser);
 
