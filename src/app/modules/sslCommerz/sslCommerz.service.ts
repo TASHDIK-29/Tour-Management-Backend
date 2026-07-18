faf7c427f21d7e754f2413fc2bc88b0d555ab95b
TASHDIK-29 <trk.ice153@gmail.com>
2026-07-19 00:35:32 +0600

    Invoice added of payment

--- src/app/modules/sslCommerz/sslCommerz.service.ts
+++ src/app/modules/sslCommerz/sslCommerz.service.ts
@@ -1,8 +1,10 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import axios from "axios"
 import httpStatus from "http-status-codes"
 import { envVars } from "../../config/env"
 import { ISSLCommerz } from "./sslCommerz.interface"
 import AppError from "../../error/AppError"
+import { Payment } from "../payment/payment.model"
 
 const sslPaymentInit = async (payload: ISSLCommerz) => {
 
@@ -55,6 +57,29 @@ const sslPaymentInit = async (payload: ISSLCommerz) => {
     }
 }
 
+
+
+const validatePayment = async (payload: any) => {
+    try {
+        const response = await axios({
+            method: "GET",
+            url: `${envVars.SSL.SSL_VALIDATION_API}?val_id=${payload.val_id}&store_id=${envVars.SSL.STORE_ID}&store_passwd=${envVars.SSL.STORE_PASS}`
+        })
+
+        console.log("sslcomeerz validate api response", response.data);
+
+        await Payment.updateOne(
+            { transactionId: payload.tran_id },
+            { paymentGatewayData: response.data },
+            { runValidators: true })
+    } catch (error: any) {
+        console.log(error);
+        throw new AppError(401, `Payment Validation Error, ${error.message}`)
+    }
+}
+
+
 export const SSLService = {
-    sslPaymentInit
+    sslPaymentInit,
+    validatePayment
 }
